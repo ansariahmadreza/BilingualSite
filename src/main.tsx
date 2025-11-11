3992ef614c30c0da7e95bb1947bedeff57189b5c
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2025-11-11 10:16:09 +0330

    change

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import  "./style.css";
+import {  Routes, Route } from "react-router-dom";
+import "./style.css";
 import Home from "./pages/Home";
 import "./i18n";
 import Languageselector from "./LanguageSelector";
 import NotFound from "./components/NotFound/PageNotFount";
 import Signup from "./components/signup/Signup";
 import DescripHeader from "./components/header/Desheader";
+import { HashRouter } from "react-router-dom"
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Languageselector/>
+    <HashRouter>
+      <Languageselector />
       <div className="mr-50 mt-8 font-bold font-stretch-semi-condensed">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/*" element={<NotFound/>} />
-          <Route path="/DescripHeader" element={<DescripHeader/>} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/*" element={<NotFound />} />
+          <Route path="/DescripHeader" element={<DescripHeader />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-    react(),         // پلاگین React برای Vite
-    tailwindcss(),   // Tailwind CSS
-    tsconfigPaths(), // استفاده از مسیرهای tsconfig
+    react(),         
+    tailwindcss(),   
+    tsconfigPaths(), 
   ],
-  
-
-
-});
+  base: mode === 'production' ? '/BilingualSite/' : '/',
+}));
